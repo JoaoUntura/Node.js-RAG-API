5bc2efa654d617f30621a0a98ea0a0301cf3b8e8
JoaoUntura <joaovitorunturasteam@gmail.com>
2025-05-12 13:01:32 -0300

    working

--- server.js
+++ server.js
@@ -5,17 +5,11 @@ import router from "./routes/router.js"
 import { createServer } from 'node:http';
 import setupSocketChat from "./configs/webSocketConfig.js";
 
-const corsOptions = {
-  origin: 'http://localhost:3001',
-  methods: ['GET', 'POST', 'DELETE' ],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
 const app = express()
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 app.use('/', router)
 
 const server = createServer(app);
