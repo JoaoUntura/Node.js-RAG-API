72eeb6b354e7d8d375bae6fd711e1f38ceef8798
JoaoUntura <joaovitorunturasteam@gmail.com>
2025-06-19 13:36:11 -0300

    Salvar alterações locais antes do merge

--- services/aiServices.js
+++ services/aiServices.js
@@ -1,16 +1,34 @@
-import ai from "../configs/togheterAiConfig.js";
+
+import dotenv from "dotenv"
+dotenv.config()
+
 class Ai{
     async generateReply(history){
         try{
          
           console.time("tempoExecucaoAI");
-          const response = await ai.chat.completions.create({
-            model:"meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
-            messages: history,
-        })
+
+          const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+            method: "POST",
+            headers: {
+              "Authorization": `Bearer ${process.env.OPEN_ROUTER}`, // Your OpenRouter API key
+         
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              "model": "meta-llama/llama-4-maverick:free",
+              "messages": history
+            })
+          });
+
+
+       
+        const data = await response.json();
+
+
         console.timeEnd("tempoExecucaoAI");
     
-        return response.choices[0].message.content
+        return data.choices[0].message.content
         }catch(error){
           console.log(error)
         }
